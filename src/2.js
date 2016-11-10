ba2e56a66ba0aaf16acb41e2676a3b75a17c031c
Sandro Turriate <sandro.turriate@gmail.com>
2016-11-09 19:49:27 -0800

    Move generated code, add lesson 5

--- src/2.js
+++ src/2.js
@@ -1,40 +1,18 @@
-function preload() {
-  game.input.keyboard.addCallbacks(this, function(event) {
-    var letter = String.fromCharCode(event.keyCode || event.which).toUpperCase();
-    keyPressed(letter);
-  });
-}
-
-function drawLetterOnScreen(letter) {
-  var text = game.add.text(game.world.centerX, game.world.centerY, letter);
-  text.anchor.set(0.5);
-  text.fontSize = 50;
-  text.fill = "#ffffff";
-  return text;
-}
-
-function dropSprite(sprite) {
-  game.physics.enable(sprite, Phaser.Physics.ARCADE);
-  sprite.enableBody = true;
-  sprite.body.velocity.y = 20;
-  sprite.checkWorldBounds = true;
-  sprite.events.onOutOfBounds.add(function(sprite) {
-    console.log("out of bounds", sprite)
-  }, this);
-}
-
-
 // LESSON ONE
-// Get the letter from the event, and show it in the console
+// Show the key pressed in the console
 function keyPressed(letter) {
   console.log(letter);
 }
+
+
 
 // LESSON TWO
 // Draw the letter to the screen
 function keyPressed(letter) {
   drawLetterOnScreen(letter);
 }
+
+
 
 // LESSON THREE
 // Clear the screen between drawing each letter
@@ -43,11 +21,26 @@ function keyPressed(letter) {
   drawLetterOnScreen(letter);
 }
 
+
+
 // LESSON FOUR
 // Drop the letter downward
-// describe a sprite
 function keyPressed(letter) {
   game.world.removeAll();
   var sprite = drawLetterOnScreen(letter);
   dropSprite(sprite);
+}
+
+
+
+// LESSON FIVE
+// Move the letter to a random spot after it falls off the screen
+// Stop clearing the screen to allow multiple letters
+function keyPressed(letter) {
+  var sprite = drawLetterOnScreen(letter);
+  dropSprite(sprite);
+}
+function outOfBounds(sprite) {
+    sprite.x = game.rnd.integerInRange(10, 700);
+    sprite.y = 100;
 }
