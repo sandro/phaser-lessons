fad38f68959c3f63fb19daf08df9dd85b188a0bf
Sandro Turriate <sandro.turriate@gmail.com>
2016-11-15 20:08:17 -0800

    Move webserver into gulp default task

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp");
 
 var concat = require("gulp-concat");
+var connect = require("gulp-connect");
 
 gulp.task("one", function() {
   return gulp.src(["1.js", "game.js"])
@@ -16,6 +17,14 @@ gulp.task("three", function() {
   return gulp.src("3.js").pipe(gulp.dest("build"));
 });
 
-gulp.task("default", ["one", "two", "three"]);
+gulp.task("compile", ["one", "two", "three"]);
 
-gulp.watch('*.js', ["default"]);
+gulp.task("watch", function() {
+  gulp.watch('*.js', ["compile"]);
+});
+
+gulp.task('webserver', function() {
+  connect.server();
+});
+
+gulp.task("default", ["compile", "webserver", "watch"]);
