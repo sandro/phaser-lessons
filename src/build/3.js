1f3815ded456b57eda68aaff0252161af828982a
Sandro Turriate <sandro.turriate@gmail.com>
2016-11-15 20:35:56 -0800

    Separate exercise three into lessons

--- src/build/3.js
+++ src/build/3.js
@@ -1,50 +1,104 @@
+// LESSON ONE
+// draw two eyes and a mouth
+function drawFace() {
+  // eyes
+  draw("O", 300, 140);
+  draw("O", 400, 140);
+  // mouth
+  draw("O", 350, 310);
+}
 
-function preload() {
+// LESSON TWO
+// add a nose
+function drawFace() {
+  // eyes
+  draw("O", 300, 140);
+  draw("O", 400, 140);
+
+  // nose
+  draw(">", 360, 210);
+
+  // mouth
+  draw("O", 350, 310);
+}
+
+// LESSON THREE
+// use stars for the mouth
+// make a simple smile
+function drawFace() {
+  // eyes
+  draw("O", 300, 140);
+  draw("O", 400, 140);
+
+  // nose
+  draw(">", 360, 210);
+
+  // mouth
+  draw("*", 300, 300);
+  draw("*", 350, 310);
+  draw("*", 400, 300);
+}
+
+// LESSON FOUR
+// make a wider smile
+function drawFace() {
+  // eyes
+  draw("O", 300, 140);
+  draw("O", 400, 140);
+
+  // nose
+  draw(">", 360, 210);
+
+  // mouth
+  draw("*", 265, 270);
+  draw("*", 300, 300);
+  draw("*", 350, 310);
+  draw("*", 400, 300);
+  draw("*", 435, 270);
+}
+
+// LESSON FIVE
+// make the eyes blink
+function drawFace() {
+  // eyes
+  leftEye = draw("O", 300, 140);
+  rightEye = draw("O", 400, 140);
+  blink(leftEye, "^", 200);
+  blink(rightEye, "^", 200);
+
+  // nose
+  draw(">", 360, 210);
+
+  // mouth
+  draw("*", 265, 270);
+  draw("*", 300, 300);
+  draw("*", 350, 310);
+  draw("*", 400, 300);
+  draw("*", 435, 270);
+}
+
+// LESSON SIX
+// draw a border around the face
+function drawFace() {
+  // eyes
+  leftEye = draw("O", 300, 140);
+  rightEye = draw("O", 400, 140);
+  blink(leftEye, "^", 200);
+  blink(rightEye, "^", 200);
+
+  // nose
+  draw(">", 360, 210);
+
+  // mouth
+  draw("*", 265, 270);
+  draw("*", 300, 300);
+  draw("*", 350, 310);
+  draw("*", 400, 300);
+  draw("*", 435, 270);
+
+  // border
   drawHorizontal("-", 180, 550, 50, 20);
   drawHorizontal("-", 180, 550, 400, 20);
   drawVertical("|", 88, 400, 180, 55);
   drawVertical("|", 88, 400, 543, 55);
-  leftEye = drawLetterOnScreen("O", 300, 140);
-  rightEye = drawLetterOnScreen("O", 400, 140);
-  blink(leftEye, "^", 200);
-  blink(rightEye, "^", 200);
-  drawLetterOnScreen(">", 360, 210);
-  drawLetterOnScreen("*", 265, 270);
-  drawLetterOnScreen("*", 300, 300);
-  drawLetterOnScreen("*", 350, 310);
-  drawLetterOnScreen("*", 400, 300);
-  drawLetterOnScreen("*", 435, 270);
-
-}
-
-function blink(sprite, character, duration, start) {
-  start = start || 2000;
-  var timer = game.time.create(false);
-  timer.loop(start,
-    function() {
-      timer.pause();
-      var oldText = sprite.text;
-      sprite.text = character;
-      var newTimer = game.time.create(true);
-      newTimer.add(duration, function() {
-        sprite.text = oldText;
-        timer.resume();
-      },
-      this);
-      newTimer.start();
-    },
-  this);
-  timer.start();
-}
-
-function drawHorizontal(letter, start, end, y, step) {
-  for (var i = start; i < end; i+= step) {
-    drawLetterOnScreen(letter, i, y);
-  }
-}
-
-function drawVertical(letter, start, end, x, step) {
-  for (var i = start; i < end; i+= step) {
-    drawLetterOnScreen(letter, x, i);
-  }
 }
