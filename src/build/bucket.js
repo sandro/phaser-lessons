450e00e98239302f761cf2216962f88507989f1b
Sandro Turriate <sandro.turriate@gmail.com>
2017-02-26 19:37:52 -0800

    Cleanup

--- src/build/bucket.js
+++ src/build/bucket.js
@@ -2,13 +2,30 @@ var sprites = [];
 var arrows, item, bucket, emitter, cloud, pipe1, pipe2, pipe3, pipe4;
 var rainDirection = 1;
 var rainSaved = 0;
-var velocity = 10;
+var velocity = 12;
 var rainAmount = 1;
-var rainLifespan = 1000;
-var MaxTime = 20;
+var rainLifespan = 1500;
+var GameTime = 20;
 var rainDrop;
 var GameScoreText,TimerText;
 var splashEmitter;
+
+var MaxSplashParticles = 40;
+var SingleSplashParticles = 20;
+var SplashLifespan = 500;
+var SplashYRange = 0;
+var SplashXRange = 0;
+
+var LeakMaxSpeed = 500;
+
+function tick() {
+  if (arrows.right.isDown) {
+    bucket.x += velocity;
+  }
+  if (arrows.left.isDown) {
+    bucket.x -= velocity;
+  }
+}
 
 function bucketCollide(bucket, particle) {
   rainSaved += 1;
@@ -16,12 +33,11 @@ function bucketCollide(bucket, particle) {
   GameScoreText.text = scoreText();
   splashEmitter.x = bucket.x + 60;
   splashEmitter.y = bucket.y;
-  splashEmitter.explode(500, 20);
-  console.log(particle);
+  splashEmitter.explode(SplashLifespan, SingleSplashParticles);
 }
 
 function scoreText() {
-  return "Rain Saved: " + rainSaved
+  return "Water Saved: " + rainSaved
 }
 
 function drawScore() {
@@ -31,7 +47,7 @@ function drawScore() {
 }
 
 function drawTimer() {
-  TimerText = game.add.text(750, 35, MaxTime);
+  TimerText = game.add.text(750, 35, GameTime);
   TimerText.fill = "yellow";
   TimerText.fontSize = 16;
 }
@@ -51,7 +67,7 @@ function update() {
     return;
   }
 
-  var timeLeft = MaxTime - Math.floor(game.time.now / 1000);
+  var timeLeft = GameTime - Math.floor(game.time.now / 1000);
   if (timeLeft <= 5) {
     TimerText.fill = "red";
   }
@@ -60,48 +76,31 @@ function update() {
   }
   TimerText.text = timeLeft;
 
-
-  if (arrows.right.isDown) {
-    bucket.x += velocity;
-  }
-  if (arrows.left.isDown) {
-    bucket.x -= velocity;
-  }
-
+  tick();
   game.physics.arcade.collide(emitter, bucket, bucketCollide);
-
-  if (emitter.x + 50 > game.world.width) {
-    rainDirection = -1;
-  } else if (emitter.x + 50 < 0) {
-    rainDirection = 1;
-  }
-  // emitter.x = emitter.x + rainDirection;
-  // cloud.x += rainDirection;
-  // rainDrop
 }
 
 function preload() {
   game.load.image('bucket', 'img/bucket.png');
-  game.load.image("cloud", "img/cloud.png");
   game.load.image("pipe", "img/pipe.png");
   var rainDrop = makeRainDrop(20);
   emitter = game.add.emitter(game.world.centerX, 30, rainAmount);
-  emitter.width = 500;
+  emitter.width = 400;
   emitter.makeParticles(rainDrop, 0, emitter.maxParticles, true, false);
   emitter.gravity = 800;
   emitter.minParticleScale = 1;
   emitter.maxParticleScale = 1;
   emitter.setXSpeed(0,5);
-  emitter.setYSpeed(350, 500);
+  emitter.setYSpeed(350, LeakMaxSpeed);
   emitter.start(false, rainLifespan, 10);
 
   splashEmitter = game.add.emitter(0, 0, 40);
   splashEmitter.width = 50;
   splashEmitter.makeParticles(makeRainDrop(10), 0);
-  splashEmitter.minParticleScale = 0.5;
+  splashEmitter.minParticleScale = 0.4;
   splashEmitter.maxParticleScale = 1;
-  splashEmitter.setYSpeed(-250, 0);
-  splashEmitter.setXSpeed(-50, 50);
+  splashEmitter.setYSpeed(-250 - SplashYRange, 0 + SplashYRange);
+  splashEmitter.setXSpeed(-50 - SplashXRange, 50 + SplashXRange);
   splashEmitter.gravity = 400;
 
   arrows = game.input.keyboard.createCursorKeys();
@@ -110,7 +109,6 @@ function preload() {
 function makePipes() {
   var pipeGroup = game.add.group();
   pipe1 = pipeGroup.create(5, 0, "pipe");
-  // pipe = game.add.sprite(0, 0, "pipe");
   pipe2 = pipeGroup.create(pipe1.x + pipe1.width - 3, 0, "pipe");
   pipe3 = pipeGroup.create(pipe2.x + pipe2.width - 3, 0, "pipe");
   pipe4 = pipeGroup.create(pipe3.x + pipe3.width - 3, 0, "pipe");
@@ -119,35 +117,22 @@ function makePipes() {
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
   bucket = game.add.sprite(30, 480, 'bucket')
-  // cloud = game.add.sprite(emitter.x - 100, 0, "cloud");
   makePipes();
   game.physics.arcade.enable(bucket);
-  // bucket.body.bounce.set(0);
   bucket.body.collideWorldBounds = true;
   bucket.body.immovable = true;
   game.physics.arcade.enable(emitter);
   drawScore();
   drawTimer();
-  // emitter.body.bounce.set(0);
-  // emitter.body.mass = 0;
-  // rainDrop.body.mass = 0;
-
-  // emitter.body.collideWorldBounds = true;
 }
 
 function makeRainDrop(radius) {
   var graphics = game.add.graphics(10, 10);
   graphics.moveTo(10, 10);
-  // graphics.lineStyle(1, 0xffffff, 0);
-  // graphics.lineTo(width, 10);
   graphics.beginFill(0xB6DAFF);
-  graphics.drawCircle(50,50,radius);
+  graphics.drawCircle(10,10,radius);
   graphics.endFill();
   txt = graphics.generateTexture();
   graphics.destroy();
   return txt;
-  // var s = game.add.sprite(x, y, txt);
-  // game.physics.arcade.enable(s);
-  // s.body.immovable = true;
-  // return s;
 }
